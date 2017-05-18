0d92b63014dea28f1e64a80929b7ccf6545ba12b
Daniel Araujo <danielaraujos@live.com>
2017-05-18 16:45:41 -0300

    Primeiro envio

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Sapiens' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  
+  pod 'Alamofire', '~> 4.4'
+  # Pods for Sapiens
+  pod 'Firebase'
+end
---
+++ Sapiens/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Sapiens
+//
+//  Created by Daniel Araújo on 18/05/17.
+//  Copyright © 2017 Daniel Araújo Silva. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+
+}
+
