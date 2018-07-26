1f354d4da767f3027ff6a01b4a8e7aac72cea4a4
Daniel Araujo <danielaraujos@live.com>
2018-07-26 07:31:14 -0300

    Criando Subject Cell - Para identificar os campos

---
+++ Sapiens/SubjectCell.swift
@@ -0,0 +1,17 @@
+//
+//  SubjectCell.swift
+//  Sapiens
+//
+//  Created by Daniel Araújo on 24/07/2018.
+//  Copyright © 2018 Daniel Araújo Silva. All rights reserved.
+//
+
+import UIKit
+
+class SubjectCell : UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var detail: UILabel!
+    
+
+}
