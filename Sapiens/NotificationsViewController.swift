79595af5b435081205f9cbbf9358e90c07b3b3f2
Daniel Araujo <danielaraujos@live.com>
2018-09-17 12:12:02 -0300

    Add Picker View in class NotificationsViewController, change layout

--- Sapiens/NotificationsViewController.swift
+++ Sapiens/NotificationsViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    var times = ["5","10","15","25","30","35","40"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
 
     
@@ -22,4 +24,19 @@ class NotificationsViewController: UIViewController {
     }
     
 
+}
+extension NotificationsViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return times.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return times[row]
+    }
+    
 }
