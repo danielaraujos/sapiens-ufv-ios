cdc2ccfb4feae1198a081055b530f14568711139
Daniel Araujo <danielaraujos@live.com>
2018-09-18 21:07:00 -0300

    Configuration PickerViewController and save offline preference

--- Sapiens/NotificationsViewController.swift
+++ Sapiens/NotificationsViewController.swift
@@ -8,22 +8,43 @@
 
 import UIKit
 
-class NotificationsViewController: UIViewController {
+class NotificationsViewController: BaseViewController {
 
+    @IBOutlet weak var isOn: UISwitch!
     @IBOutlet weak var pickerView: UIPickerView!
     var times = ["5","10","15","25","30","35","40"]
+    var itemAtDefaultPosition: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.delegate = self
         pickerView.dataSource = self
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //RESPONSAVEL POR DEFINIR SE O SWICTH ESTÁ ATIVO
+        if defaults.bool(forKey: REST.localStorageNotifications) == true {
+            isOn.isOn = true
+        }else{
+            isOn.isOn = false
+        }
+        
+        // RESPONSAVEL POR SALVAR O TEMPO
+        if let index = times.index(of: String(defaults.integer(forKey: REST.localStorageTime))){
+            self.pickerView.selectRow(index, inComponent: 0, animated: true)
+        }
+    }
+    
+    @IBAction func btIsOn(_ sender: UISwitch) {
+        if isOn.isOn == true {defaults.set(true, forKey: REST.localStorageNotifications)
+        }else {defaults.set(false, forKey: REST.localStorageNotifications)}
+    }
     
     @IBAction func btnCancel(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil);
     }
-    
-
 }
 extension NotificationsViewController : UIPickerViewDelegate, UIPickerViewDataSource {
     
@@ -39,4 +60,7 @@ extension NotificationsViewController : UIPickerViewDelegate, UIPickerViewDataSo
         return times[row]
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        defaults.set(Int(times[row]), forKey: REST.localStorageTime)
+    }
 }
