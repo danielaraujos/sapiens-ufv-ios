b0e6759258a446f91e49934752f0f6f893ba47a0
Daniel Araujo <danielaraujos@live.com>
2018-09-27 21:57:06 -0300

    Create a function Coredata and Protocol

---
+++ Sapiens/CoreDataManager.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataManager.swift
+//  Sapiens
+//
+//  Created by Daniel Araújo on 27/09/2018.
+//  Copyright © 2018 Daniel Araújo Silva. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol CoreDataManagerType {
+    func saveUser(user: User)
+    func returnUser() -> User
+    func deleteUser()
+    //func fetchTitles() -> [NSManagedObject]
+}
+
+class CoreDataManager {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Sapiens")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
